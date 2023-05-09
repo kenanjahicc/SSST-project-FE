fc0cdc4326e5015580c7b8d350681b6d6981830d
Malik <malik.krijestorac@stu.ssst.edu.ba>
2023-05-09 09:38:07 +0200

    Added new role component with html template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-
+import { NewRoleComponent } from './new-role/new-role.component';
+import { NewRoleEditComponent } from './new-role-edit/new-role-edit.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewRoleComponent,
+    NewRoleEditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatFormFieldModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
