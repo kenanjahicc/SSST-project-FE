265aa5c3b06c1842b14a3701486e09884e0251ef
salihmusic <salih.music@stu.ssst.edu.ba>
2023-05-10 21:20:45 +0200

    Nav Bar - Finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,31 +2,28 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule,Routes} from "@angular/router";
 import {TeamsComponent} from "./teams/teams.component";
-import {Routes} from "@angular/router";
 import {NewRoleComponent} from "./new-role/new-role.component";
 import {NewRoleEditComponent} from "./new-role-edit/new-role-edit.component";
 
 
-
 const routes: Routes = [
   {
-    path: "/new-role",
+    path: "new-role",
     component: NewRoleComponent
   },
   {
-    path: "/new-role-edit",
+    path: "new-role-edit",
     component: NewRoleEditComponent
-  }
-]
-
-const routes: Routes = [
+  },
   {
     path: 'teams',
     component:TeamsComponent
-  },
-];
+  }
+]
+
+
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), CommonModule],
   exports: [RouterModule],
   declarations: [],
 
